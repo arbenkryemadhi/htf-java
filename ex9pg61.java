9c3732d615639e80b78389dc52fa031a19e93a85
Arben Kryemadhi <arbenofc@gmail.com>
2024-09-18 00:05:18 +0200

    a

---
+++ ex9pg61.java
@@ -0,0 +1,8 @@
+public class ex9pg61 {
+    public static void main(String[] args) {
+        System.out.println("If you want space, I hope you take your helmet off soon as you get up to Venus and choke");
+        System.out.println("Even while sleepin', be thinkin' so woke");
+        System.out.println("White Cartiers, all I'm seein' is dope");
+        System.out.println("My city Leia and I'm Obi-Wan, got a mission to pass, I just might be the hope");
+    }
+}
