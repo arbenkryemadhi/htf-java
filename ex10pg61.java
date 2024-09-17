7e0f4a2f64c71d27d8811ab82b6d55c5af362027
Arben Kryemadhi <arbenofc@gmail.com>
2024-09-18 00:07:53 +0200

    a

---
+++ ex10pg61.java
@@ -0,0 +1,8 @@
+public class ex10pg61 {
+    public static void main(String[] args) {
+        // Program comments are nonexecuting statements you add to the a file for the
+        // puropose of documentation."
+        System.out.println(
+                "Program comments are nonexecuting statements you add to the a file for the puropose of documentation.");
+    }
+}
